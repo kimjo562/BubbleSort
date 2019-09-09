051d19eb97690e73f0867943f1db01df1fcface9
Joseph Kim <s198022@students.aie.edu>
2019-09-09 16:13:04 -0500

    Reverse added other sorts

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -8,21 +8,41 @@ namespace BubbleSort
 {
     class Program
     {
+        static int passes = 0;
+        static int swaps = 0;
+        static int loops = 0;
+
         static void Main(string[] args)
         {
 
-            bool sorting = true;
 
-//            int[] arrayToSort = { 45, 6, 12, 51, 4, 23, 31 };
+
+            //            int[] arrayToSort = { 45, 6, 12, 51, 4, 23, 31 };
             int[] arrayToSort = {14, 65, 63, 1, 54, 89, 84, 9, 98, 57,
                                  71, 18, 21, 84, 69, 29, 11, 83, 13, 42,
                                  64, 58, 78, 82, 13, 9, 96, 14, 39, 89,
                                  40, 32, 51, 85, 48, 40, 23, 15, 94, 93,
                                  35, 81, 1, 9, 43, 39, 15, 17, 97, 52};
-            int temp;
-            int swaps = 0;
-            int loops = 0;
 
+
+
+            //Bubble Sort
+            CocktailShakerSort(arrayToSort);
+
+            Console.WriteLine("\nPasses: " + passes);
+            Console.WriteLine("Swaps: " + swaps);
+            Console.WriteLine("Loops: " + loops);
+            Console.ReadKey();
+
+        }
+
+
+
+
+        public static void BubbleSort(int[] arrayToSort)
+        {
+            bool sorting = true;
+            int temp;
 
             Console.WriteLine("Unsorted ");
             Console.WriteLine("---------------------------------");
@@ -40,6 +60,7 @@ namespace BubbleSort
                 sorting = false;
                 for (int i = 1; i < arrayToSort.Length; i++)
                 {
+
                     if (arrayToSort[i] < arrayToSort[i - 1])
                     {
                         temp = arrayToSort[i];
@@ -60,11 +81,127 @@ namespace BubbleSort
             {
                 Console.Write(arrayToSort[i] + " ");
             }
-            Console.WriteLine("\nSwaps: " + swaps);
-            Console.WriteLine("Loops: " + loops);
-            Console.ReadKey();
 
         }
+
+
+        public static void OptimizedBubbleSort(int[] arrayToSort)
+        {
+            bool sorting = true;
+            int temp;
+            int magic = arrayToSort.Length;
+
+            Console.WriteLine("Unsorted ");
+            Console.WriteLine("---------------------------------");
+
+            for (int i = 0; i < arrayToSort.Length; i++)
+            {
+                Console.Write(arrayToSort[i] + " ");
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            while (sorting)
+            {
+                sorting = false;
+                passes++;
+                for (int i = 1; i < magic; i++)
+                {
+                    if (arrayToSort[i] < arrayToSort[i - 1])
+                    {
+                        temp = arrayToSort[i];
+                        arrayToSort[i] = arrayToSort[i - 1];
+                        arrayToSort[i - 1] = temp;
+                        swaps++;
+                        sorting = true;
+                    }
+                    loops++;
+                }
+                magic--;
+
+            }
+
+            Console.WriteLine("Sorted ");
+            Console.WriteLine("---------------------------------");
+            for (int i = 0; i < arrayToSort.Length; i++)
+            {
+                Console.Write(arrayToSort[i] + " ");
+            }
+
+        }
+
+
+
+
+        public static void CocktailShakerSort(int[] arrayToSort)
+        {
+            bool sorting = true;
+            int temp;
+            int magic = arrayToSort.Length - 1;
+            int chickenNumber = 0;
+
+            Console.WriteLine("Unsorted ");
+            Console.WriteLine("---------------------------------");
+
+            for (int i = 1; i < arrayToSort.Length; i++)
+            {
+                Console.Write(arrayToSort[i] + " ");
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+
+            while (sorting)
+            {
+                sorting = false;
+                passes++;
+                for (int i = chickenNumber; i < magic; i++)
+                {
+                    if (arrayToSort[i] > arrayToSort[i + 1])
+                    {
+                        temp = arrayToSort[i];
+                        arrayToSort[i] = arrayToSort[i + 1];
+                        arrayToSort[i + 1] = temp;
+                        swaps++;
+                        sorting = true;
+                    }
+                    loops++;
+                }
+                magic--;
+
+
+                for (int i = magic; i > chickenNumber; i--)
+                {
+                    if (arrayToSort[i] < arrayToSort[i - 1])
+                    {
+                        temp = arrayToSort[i];
+                        arrayToSort[i] = arrayToSort[i - 1];
+                        arrayToSort[i - 1] = temp;
+                        swaps++;
+                        sorting = true;
+                    }
+                    loops++;
+                }
+                chickenNumber++;
+
+            }
+
+
+            Console.WriteLine("Sorted ");
+            Console.WriteLine("---------------------------------");
+            for (int i = 0; i < arrayToSort.Length; i++)
+            {
+                Console.Write(arrayToSort[i] + " ");
+            }
+
+        }
+
     }
 }
+
+
+
+
+
 
